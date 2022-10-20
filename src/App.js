b86f6045201e75fdc003cd7dabe89635532488d6
sentientrock <ah88238@uga.edu>
2022-10-20 18:23:31 -0400

    Changed style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
 import CountdownTimer from './CountdownTimer'
+import { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+const CONDATE = new Date("February 25, 2023 12:00:00").getTime();
 
 function App() {
 
-  const CONDATE = new Date("February 25, 2023 12:00:00").getTime();
+  return (
+    <div>
+      <Routes>
+        <Route path="/test" element={<Test />} />
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </div>
+  );
+}
+
+function Home() {
+
+  const [timerLoaded, setTimerLoaded] = useState(false);
+  useEffect(() => {
+    setTimerLoaded(true);
+  }, [])
 
   return (
-    <div className="App">
+    <div className="Home">
       <img className='wcclogo' 
       src={require("./images/wcclogo.png")} alt={"WccLogo"}/>
       <h1 className='title'>
@@ -15,6 +39,10 @@ function App() {
       <CountdownTimer targetDate={CONDATE} />
     </div>
   );
+}
+
+function Test() {
+  return <div> Hello World !</div>
 }
 
 export default App;
